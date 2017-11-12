27ab86e35cdfb1a8b91e25924deade692cc428e1
Julianne Kim <juliannne@gmail.com>
2017-11-12 04:18:57 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,9 @@ class Application
     # resp.write "Hello, World"
     # resp.finish
 
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
+    num_1 = Kernel.rand(1..2)
+    num_2 = Kernel.rand(1..2)
+    num_3 = Kernel.rand(1..2)
 
     resp.write "#{num_1}\n"
     resp.write "#{num_2}\n"
